5a55d7ae2dd12056f302121e3ab6f743f838f552
manojkrjoshi <manojkr.joshi@gmail.in>
2022-08-23 08:05:09 +0530

    new service details rest API included

--- src/com/synectiks/json/filter/restclient/RestClient.java
+++ src/com/synectiks/json/filter/restclient/RestClient.java
@@ -102,10 +102,122 @@ public class RestClient {
 			  
 		 }
 
-		}
+	}
+	
+	public static void transformServiceDetails() {
+		String API_END_POINT = BASE_URL + "/service-detail/transform";
+		HttpURLConnection conn = null;
+	  	try {
+			  	URL url = new URL(API_END_POINT);
+				conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+					
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+				}
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+		  }catch (MalformedURLException e) {
+			  e.printStackTrace();
+		  }catch (IOException e) {
+			  e.printStackTrace();
+		  }finally {
+			 if(conn != null) {
+				conn.disconnect();
+			 }
+		  }
+	}
+	
+	public static void searchWithFiltersServiceDetails() {
+		String API_END_POINT = BASE_URL + "/service-detail/search-with-filter";
+		HttpURLConnection conn = null;
+	  	try {
+			  	URL url = new URL(API_END_POINT);
+				conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+					
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+				}
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+		  }catch (MalformedURLException e) {
+			  e.printStackTrace();
+		  }catch (IOException e) {
+			  e.printStackTrace();
+		  }finally {
+			 if(conn != null) {
+				conn.disconnect();
+			 }
+		  }
+	}
+	
+	
+	public static void createBulkDataServiceDetail() {
+		String API_END_POINT = BASE_URL + "/service-detail/create-bulk-data";
+		HttpURLConnection conn = null;
+		  try {
+
+			URL url = new URL(API_END_POINT);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+			String input = JsonFilter.loadJson("C:\\mycode\\json-data\\services.json");
+			try(OutputStream os = conn.getOutputStream()) {
+				byte[] byteInput = input.getBytes("utf-8");
+				os.write(byteInput, 0, byteInput.length);	
+				os.flush();		
+			}
+			catch(Exception e)
+			{
+				System.out.println(e.getMessage());
+			}
+
+//			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED && conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//				throw new RuntimeException("Failed : HTTP error code : "
+//					+ conn.getResponseCode());
+//			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+		  } catch (MalformedURLException e) {
+			e.printStackTrace();
+		  } catch (IOException e) {
+			e.printStackTrace();
+		 }finally {
+			 if(conn != null) {
+				 conn.disconnect();
+			 }
+		 }
+
+	}
 	
 	public static void main(String a[]) {
 		//testGetApi();
-		testUpdateApi();
+//		testUpdateApi();
+		createBulkDataServiceDetail();
 	}
 }
